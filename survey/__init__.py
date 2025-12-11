21075d14cdcb716a3b6327a141091adfbb9b19fb
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2025-12-11 11:13:05 +0100

    Legger til fil for analyse av spørreskjemaet. -
    Kopiert fra tilgjengelige-informasjonskapsler.

---
+++ survey/__init__.py
@@ -0,0 +1,7 @@
+"""
+  Created on 2025-12-11.
+  
+  Last updated on 2024-12-11.
+  
+  This package is used to analys the results from the survey in the project Tilgjengelige informasjonskapsler, conducted in October and November of 2024.
+"""
